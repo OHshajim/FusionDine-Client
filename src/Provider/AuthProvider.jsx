e78ed5c9f231af2a1bf1ea6d02d9f8d25c59f70f
OHshajim <ajshajimmax@gmail.com>
2024-05-10 15:03:11 +0600

    login  add

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,14 +1,48 @@
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../Firebase/Firebase.config";
 
 export const AuthContext = createContext(null)
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({})
+  const [loading, setLoad] = useState(true)
 
-    const info = { }
-    return (
-        <AuthContext.Provider value={info}>
-          {children}
-        </AuthContext.Provider>
-      );
+
+  // create user 
+  const createUser = (email, password) => {
+    setLoad(true)
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+  // login 
+  const Login = (email, password) => {
+    setLoad(true)
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  // Login with google and github
+  const loginWithGoogle = (provider) => {
+    setLoad(true);
+    return signInWithPopup(auth, provider);
+  }
+  // Logout
+  const Logout = () => {
+    setLoad(true)
+    signOut(auth)
+  }
+  useEffect(() => {
+    const currentUser = onAuthStateChanged(auth, (user) => {
+      setUser(user)
+      setLoad(false);
+    })
+    return () => currentUser();
+  }, [])
+
+  const info = { user, createUser, Login, Logout, loginWithGoogle, loading }
+  return (
+    <AuthContext.Provider value={info}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home";
 import ErrorPage from "../Pages/ErrorPage";
+import Login from "../Pages/Login";
 
 const Router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ const Router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
+            },
+            {
+                path: "/login",
+                element: <Login/>,
             },
         ]
     },
