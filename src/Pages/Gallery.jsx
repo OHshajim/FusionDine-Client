5faf81d28f32919cc7cdfb02eea24116ac566a6c
OHshajim <ajshajimmax@gmail.com>
2024-05-11 18:37:01 +0600

    add model on gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -35,7 +35,66 @@ const Gallery = () => {
                 }
             </div>
             <button className=" fixed  bottom-8 right-8 btn btn-outline rounded-full font-bold text-base"><span> <GrAdd /></span> Add</button>
-            
+            <div className="relative flex justify-center">
+                <button className="px-6 py-2 mx-auto tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                    Open Modal
+                </button>
+
+                <div
+                    //             {x - show="isOpen"
+                    //         x-transition:enter="transition duration-300 ease-out"
+                    //         x-transition:enter-start="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
+                    //         x-transition:enter-end="translate-y-0 opacity-100 sm:scale-100"
+                    //         x-transition:leave="transition duration-150 ease-in"
+                    //         x-transition:leave-start="translate-y-0 opacity-100 sm:scale-100"
+                    // x-transition:leave-end="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"}
+                    className="fixed inset-0 z-10 overflow-y-auto"
+                    aria-labelledby="modal-title" role="dialog" aria-modal="true"
+                >
+                    <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+                        <span className="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true">&#8203;</span>
+
+                        <div className="relative inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl dark:bg-gray-900 sm:my-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
+                            <h3 className="text-lg font-medium leading-6 text-gray-800 capitalize dark:text-white" id="modal-title">
+                                Add Your Opinion
+                            </h3>
+                            <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+                                Your new project has been created. Invite your
+                                team to collaborate on this project.
+                            </p>
+
+                            <form className="mt-4" action="#">
+                                <label className="text-sm text-gray-700 dark:text-gray-200">
+                                    User Name
+                                </label>
+
+                                <label className="block mt-3" >
+                                    <input type="text" name="name" value=""
+                                        className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+                                </label>
+
+                                <label className="block mt-3">
+                                    <input type="text" name="feedback" className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+                                </label>
+
+                                <label className="block mt-3" >
+                                    <input type="text" name="image" placeholder="Image URL" className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+                                </label>
+
+                                <div className="mt-4 sm:flex sm:items-center sm:-mx-2">
+                                    <button type="button" className="w-full px-4 py-2 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md sm:w-1/2 sm:mx-2 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40">
+                                        Cancel
+                                    </button>
+
+                                    <button type="button" className="w-full px-4 py-2 mt-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
+                                        Add
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div >
     );
 };
